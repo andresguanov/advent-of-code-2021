058eecb3ecd98ee3f57edf8e26d7e4582aa87f2b
andresguanov <andresguanov@gmail.com>
2021-12-07 15:51:45 -0500

    Complete Day 07

---
+++ Day-07/day07.js
@@ -0,0 +1,51 @@
+import { data } from "./data"
+
+const part1 = (positions) => {
+
+    let fuel = 0;
+    const indexes = positions.sort((a, b) => a - b)
+    const length = indexes.length
+
+    positions.forEach(position => {
+        fuel += Math.abs(position - indexes[length / 2])
+    })
+
+    return fuel;
+
+}
+
+part1(data)
+
+//Part two
+
+const part2 = (positions) => {
+
+    let fuel = 0;
+    const indexes = [...new Set(positions)].sort((a, b) => a - b)
+
+    for (let i = 0; i < indexes.length; i++) {
+
+        let fuelIndex = 0;
+
+        for (let j = 0; j < positions.length; j++) {
+
+            //formula S(n)=n(n+1)/2
+            const n = Math.abs(positions[j] - indexes[i])
+            fuelIndex += n * (n + 1) / 2
+
+        }
+
+        if (i === 0 || fuelIndex < fuel) {
+
+            fuel = fuelIndex
+
+        } else {
+
+            break
+        }
+    }
+
+    return fuel;
+}
+
+part2(data)//104822130
